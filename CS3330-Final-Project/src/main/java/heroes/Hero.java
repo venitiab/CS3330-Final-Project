381cbae0100c3e2810ea92c5c84c14b443de70b9
venitiab <vhbvdd@umsystem.edu>
2024-05-06 23:59:04 -0500

    began to create abstract method to pick counter swap

--- CS3330-Final-Project/src/main/java/heroes/Hero.java
+++ CS3330-Final-Project/src/main/java/heroes/Hero.java
@@ -7,4 +7,26 @@ public abstract class Hero {
 	protected String ability1;
 	protected String ability2;
 	protected String ultimate;
+	
+	//constructor
+    public Hero(String type, String name, String weapon, String ability1, String ability2, String ultimate) {
+        this.type = type;
+        this.name = name;
+        this.weapon = weapon;
+        this.ability1 = ability1;
+        this.ability2 = ability2;
+        this.ultimate = ultimate;
+    }
+    
+    public abstract Hero pickCounter();
+    
+    //display hero information
+    public void displayInfo() {
+        System.out.println("Type: " + type);
+        System.out.println("Name: " + name);
+        System.out.println("Primary/Secondary Weapon: " + weapon);
+        System.out.println("Ability 1: " + ability1);
+        System.out.println("Ability 2: " + ability2);
+        System.out.println("Ultimate: " + ultimate);
+    }
 }
--- CS3330-Final-Project/src/main/java/heroes/Tank.java
+++ CS3330-Final-Project/src/main/java/heroes/Tank.java
@@ -1,70 +1,27 @@
 package heroes;
 
 public class Tank extends Hero {
-	
-	public Tank(String name, String weapon, String ability1, String ability2, String ultimate) {
-		
-		super();
-		this.name = name;
-		this.weapon = weapon;
-		this.ability1 = ability1;
-		this.ability2 = ability2;
-		this.ultimate = ultimate;
-		
-	}
-	//copy constructor
-	public Tank(Tank tank) { 
-		super();
-		this.name = tank.name;
-		this.weapon = tank.weapon;
-		this.ability1 = tank.ability1;
-		this.ability2 = tank.ability2;
-		this.ultimate = tank.ultimate;
-	}
-	
-	public String getName() {
-		return name;
-	}
+    public Tank(String name, String weapon, String ability1, String ability2, String ultimate) {
+        super("Tank", name, weapon, ability1, ability2, ultimate);
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getWeapon() {
-		return weapon;
-	}
+    @Override
+    public Hero pickCounter() {
+    	if (getName().equalsIgnoreCase("Reinhardt")) {
+            
+            for (Tank tank : heroList) {
+                if (tank.getName().equalsIgnoreCase("Ramattra")) {
+                    
+                    System.out.println(tank);
+                    return tank; // 
+                }
+            }
+        }
+        return null;
+    }
+    //public String toString() {
+        //return "Tank [Name = " + name + ", Primary/Secondary Weapon = " + weapon + ", Ability1 = " + ability1 + ", Ability2 = " + ability2 + ", Ultimate Ability = "
+                //+ ultimate + "]";
+    //}
 
-	public void setWeapon(String weapon) {
-		this.weapon = weapon;
-	}
-	
-	public String getAbility1() {
-		return ability1;
-	}
-
-	public void setAbility1(String ability1) {
-		this.ability1 = ability1;
-	}
-	
-	public String getAbility2() {
-		return ability2;
-	}
-
-	public void setAbility2(String ability2) {
-		this.ability2 = ability2;
-	}
-	
-	public String getUltimate() {
-		return ultimate;
-	}
-
-	public void setUltimate(String ultimate) {
-		this.ultimate = ultimate;
-	}
-	
-	@Override
-	public String toString() {
-		return "Tank [Name = " + name + ", Primary/Secondary Weapon = " + weapon + ", Ability1 = " + ability1 + ", Ability2 = " + ability2 + ", Ultimate Ability = "
-				+ ultimate + "]";
-		}
 }
